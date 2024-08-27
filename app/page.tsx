12f25ea27ec2c72d50f991d9c6ae60d1e810cb52
Lathish18 <lathish1802@gmail.com>
2024-08-27 19:34:33 +0530

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,115 @@
-import Image from "next/image";
+'use client'
+import React, { useEffect, useRef, useState } from 'react'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+type Todo = {
+  id: number;
+  text: string;
 }
+
+const ToDo = () => {
+  const [todoList, setTodoList] = useState<Todo[]>(() => {
+    const savedTodos = localStorage.getItem('todos');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isEditing, setIsEditing] = useState(null)
+  const [editText, setEditText] = useState('')
+
+  const addText = () => {
+    const inputText = inputRef.current?.value.trim();
+    
+    if (!inputText) return;
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: inputText,
+    };
+
+    setTodoList((prev) => [...prev, newTodo]);
+    if (inputRef.current) inputRef.current.value = '';
+  }
+
+  const deleteTodo = (id: number) => {
+    setTodoList((prev) => prev.filter((todo) => todo.id !== id));
+  }
+
+  const editTodo = (id:any) => {
+    const toEdit = todoList.find(todo => todo.id === id)
+    if (toEdit) {
+      setIsEditing(id)
+      setEditText(toEdit.text)
+    }
+  }
+  const saveEdit = (id:any) => {
+    if (!editText.trim()) return null
+
+    setTodoList((pre) => pre.map((todo) =>
+      todo.id === id ? { ...todo, text: editText } : todo
+    ))
+    setIsEditing(null)
+    setEditText('')
+  }
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todoList));
+  }, [todoList]);
+
+  return (
+    <div className='w-full h-screen flex items-center justify-center p-8'>
+      <div className="w-full sm:w-[520px] rounded-[20px] text-center p-5 flex flex-col gap-6 bg-[#001529] text-[#d1d5db]">
+        <div className="text-3xl font-semibold">To-Do List</div>
+        <div className="rounded-full flex items-center bg-[#1f2940]">
+          <input 
+            ref={inputRef}
+            type="text" 
+            placeholder='Type your task'
+            className='border-none outline-none p-3 bg-transparent flex-1'
+          />
+          <button 
+          className='p-3 px-6 rounded-full' onClick={addText}
+          style={{ backgroundImage: 'linear-gradient(90deg, #9333ea, #d14fdf)' }}
+          >
+            Add
+          </button>
+        </div>
+
+        {todoList.length > 0 ? <div>
+          {todoList.map((item) => (
+            <div className='flex items-center justify-between px-4 py-2'>
+              <input 
+                type="text" 
+                value={isEditing === item.id ? editText : item.text}
+                readOnly={isEditing === item.id ? false : true} 
+                onChange={(e) => setEditText(e.target.value)} 
+                className={`bg-transparent outline-none w-1/2  ${isEditing === item.id ? 'border-b' : ''}`}
+              />
+      
+              <div className="flex items-center gap-4">
+                {isEditing === item.id ? (
+                  <button 
+                  className='cursor-pointer text-purple-600' 
+                  onClick={() => saveEdit(item.id)}>
+                    Update
+                  </button>
+                ) : (
+                  <>
+                    <button
+                    className='cursor-pointer text-purple-600' 
+                    onClick={() => editTodo(item.id)}
+                    >Edit</button>
+                    <button
+                    className='cursor-pointer text-purple-600' 
+                    onClick={() => deleteTodo(item.id)}
+                    >Delete</button>
+                  </>
+                  )}
+              </div>
+            </div>
+          ))}
+        </div> : <div className="opacity-50">Add your To-Do</div>}
+      </div>
+    </div>
+  )
+}
+
+export default ToDo;
